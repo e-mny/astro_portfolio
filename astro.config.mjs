a4cbae6da6716c1852ed0b683ffcc76b9083dcb2
Enoch Mok <enochmok@Enochs-MBP.local>
2024-09-06 16:36:40 +0800

    removed jawg private token

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import vercel from "@astrojs/vercel/serverless";
